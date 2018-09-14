a635f3bcd30c162de8614690c59fdd11aaaf3c9b
steve <chph13420146901@gmail.com>
2018-09-14 15:45:15 +0800

    add some colors.xml to build pink-style theme

---
+++ app/src/main/java/com/tql/huaweiapp/activity/RegisterOrLoginActivity.java
@@ -0,0 +1,15 @@
+package com.tql.huaweiapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tql.huaweiapp.R;
+
+public class RegisterOrLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_or_login);
+    }
+}
