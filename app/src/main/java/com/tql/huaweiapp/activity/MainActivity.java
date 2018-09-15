0cc81afc1e52ca31b130559042ab855e461c64e2
Steve <chph13420146901@gmail.com>
2018-09-15 23:44:00 +0800

    主界面布局一部分

--- app/src/main/java/com/tql/huaweiapp/activity/MainActivity.java
+++ app/src/main/java/com/tql/huaweiapp/activity/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.tql.huaweiapp.R;
 
@@ -12,6 +14,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ImageView avatarImageview;
     private SlidingPaneLayout slidePanel;
+    /**
+     * 暂无收藏记录
+     */
+    private TextView noFavoriteTextview;
+    private LinearLayout favoreitesLinearlayout;
+    /**
+     * 暂无聊天记录
+     */
+    private TextView noRecordsTextview;
+    private LinearLayout otherChatListLinearlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         avatarImageview = findViewById(R.id.avatar_imageview);
         avatarImageview.setOnClickListener(this);
         slidePanel = findViewById(R.id.slide_panel);
+        avatarImageview = findViewById(R.id.avatar_imageview);
+        noFavoriteTextview = findViewById(R.id.no_favorite_textview);
+        favoreitesLinearlayout = findViewById(R.id.favoreites_linearlayout);
+        noRecordsTextview = findViewById(R.id.no_records_textview);
+        otherChatListLinearlayout = findViewById(R.id.other_chat_list_linearlayout);
+        slidePanel = findViewById(R.id.slide_panel);
+
+
     }
 
     @Override
