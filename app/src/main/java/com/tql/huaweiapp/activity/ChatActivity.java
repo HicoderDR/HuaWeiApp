1462ba071e4c9621b06e68d491a03de52b0d5638
Steve <chph13420146901@gmail.com>
2018-09-17 19:27:40 +0800

    消息输入工具布局

--- app/src/main/java/com/tql/huaweiapp/activity/ChatActivity.java
+++ app/src/main/java/com/tql/huaweiapp/activity/ChatActivity.java
@@ -2,14 +2,44 @@ package com.tql.huaweiapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.tql.huaweiapp.R;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private ImageView backImageview;
+    /**
+     * 这是名字
+     */
+    private TextView characterNameTextview;
+    private ImageView characterInfoImageview;
+    private ImageView microPhoneImageview;
+    private EditText messageEdittext;
+    private LinearLayout inputAreaLinearlayout;
+    private ImageView sendMessageImageview;
+    private LinearLayout messageInputLinearlayout;
+    private LinearLayout voiceRecordLinearlayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        initView();
+    }
+
+    private void initView() {
+        backImageview = findViewById(R.id.back_imageview);
+        characterNameTextview = findViewById(R.id.character_name_textview);
+        characterInfoImageview = findViewById(R.id.character_info_imageview);
+        microPhoneImageview = findViewById(R.id.micro_phone_imageview);
+        messageEdittext = findViewById(R.id.message_edittext);
+        inputAreaLinearlayout = findViewById(R.id.input_area_linearlayout);
+        sendMessageImageview = findViewById(R.id.send_message_imageview);
+        messageInputLinearlayout = findViewById(R.id.message_input_linearlayout);
+        voiceRecordLinearlayout = findViewById(R.id.voice_record_linearlayout);
     }
 }
