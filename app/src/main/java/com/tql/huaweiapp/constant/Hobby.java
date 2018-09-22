d696a3ea9b7d8e0e9560ad67068bd622de6b65be
steve <chph13420146901@gmail.com>
2018-09-22 11:12:39 +0800

    个性标签

--- app/src/main/java/com/tql/huaweiapp/constant/Hobby.java
+++ app/src/main/java/com/tql/huaweiapp/constant/Hobby.java
@@ -2,5 +2,6 @@ package com.tql.huaweiapp.constant;
 
 public class Hobby {
     public static final String[] HOBBIES = {
-            "兴趣2", "兴趣161", "兴趣111", "兴趣121", "兴趣13214", "兴趣8","Java","C","C++","Python","C#","JavaScript","Kotlin"    };
+            "兴趣2", "兴趣161", "兴趣111", "兴趣121", "兴趣13214", "兴趣8", "Java", "C", "C++", "Python", "C#", "JavaScript", "Kotlin"
+    };
 }
